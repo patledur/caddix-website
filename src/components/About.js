3355aa7c24332efba67210a9c7abe35b132107de
Patrick Montheu <thuspatrick@gmail.com>
2021-02-25 20:51:13 +0100

    setting up tailwindcss

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,8 @@ const About = () => {
         <div>
             <h2>About CADDIXSOLUTIONS</h2>
             <p>Caddix solutions is a collective of piping designers, who will offer you the best piping design and PDMS  courses</p>
-            <p>Piping Desing Course (systems) are like arteries and veins. They carry the lifeblood of modern civilization. Similarly, pipelines carry crude oil from oil wells to tank farms for storage or to reﬁneries for processing. The natural gas transportation and distribution lines convey natural gas from the source and storage tank forms to points of utilization, such as :
-
-power plants,industrial facilities, commercial,residential communities.In chemical plants, paper mills, food processing plants, and other similar industrial establish- ments, the piping systems are utilized to carry liquids, chemicals, mixtures, gases, vapors, and solids from one location to another. 
-
-</p>
+            <p>Piping Desing Course (systems) are like arteries and veins. They carry the lifeblood of modern civilization. Similarly, pipelines carry crude oil from oil wells to tank farms for storage or to reﬁneries for processing. The natural gas transportation and distribution lines convey natural gas from the source and storage tank forms to points of utilization, such as : power plants,industrial facilities, commercial,residential communities.In chemical plants, paper mills, food processing plants, and other similar industrial establish- ments, the piping systems are utilized to carry liquids, chemicals, mixtures, gases, vapors, and solids from one location to another. 
+            </p>
         </div>
     )
 }
